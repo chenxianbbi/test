b9191536959d8f22653c00842978bbeb013d83fe
chenxianbbi <chenxianbbi@163.com>
2020-04-15 00:35:45 +0800

    cpp test

---
+++ cppTest.cpp
@@ -0,0 +1,13 @@
+int main(int argc,char** argv) {
+	src = imread("F:/OneDrive - 汕头大学/Test Temp/opencv/part03/case04.jpg");
+	//判断是否成功读入
+	if (src.empty())
+	{
+		printf("could not load image...");
+		return -1;
+	}
+	imshow("inputImage", src);
+
+	waitKey(0);
+	return 0;
+}
